5d3494ca778bd5ec35062585e2fd6cc32d86ac17
okawahiroto <okawa.hiroto@gmail.com>
2021-01-17 07:18:24 +0900

    年齢追加

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,15 @@
 // let nameOutputText = document.getElementById('nameOutputText');
 // let btnSubmit = document.getElementById('btnSubmit');
 // let btnDelete = document.getElementById('btnDelete');
+
 nameInput.value = '';
-let userName = nameInput.value;
 // ここでuserNameを定義、代入しても、下の関数内では使えない？
+let userName = nameInput.value;
+
 console.log(userName);
+
+// formの要素取得勉強中
+console.log(form[1].value);
 
 btnSubmit.addEventListener('click', function() {
   console.log('送信ボタン');
